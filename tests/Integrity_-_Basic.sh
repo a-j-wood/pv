678f6551163e8cac0a5afaff9f8bfe5505fd462a
Andrew Wood <ivarch657@gmail.com>
2023-07-25 21:21:02 +0100

    Also test basic integrity with --force

--- tests/Integrity_-_Basic.sh
+++ tests/Integrity_-_Basic.sh
@@ -8,8 +8,20 @@ testSubject="${testSubject:-false}"
 inputString="TESTING"
 outputString=$(printf "%s" "${inputString}" | "${testSubject}" 2>/dev/null) || { echo "unexpected failure code"; exit 1; }
 
-test "${inputString}" = "${outputString}" && exit 0
-echo "output did not match input"
-exit 1
+if ! test "${inputString}" = "${outputString}"; then
+	echo "output did not match input"
+	exit 1
+fi
+
+# Test again with --force.
+inputString="TESTINGAGAIN"
+outputString=$(printf "%s" "${inputString}" | "${testSubject}" -f 2>/dev/null) || { echo "unexpected failure code"; exit 1; }
+
+if ! test "${inputString}" = "${outputString}"; then
+	echo "output did not match input when using --force"
+	exit 1
+fi
+
+exit 0
 
 # EOF
